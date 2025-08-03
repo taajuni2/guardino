91dcba0d08a823e102303641dfa3006178bef124
taajuni2 <nicolas.julier@swisscom.com>
2025-08-03 16:57:15 +0200

    added default running fastAPI

--- client/guardino-backend/app/config/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    DEBUG = True
-    HOST = "0.0.0.0"
-    PORT = int(os.getenv("FLASK_RUN_PORT", 3000))
--- client/guardino-backend/app/routes/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-ping_bp = Blueprint('ping', __name__)
-
-@ping_bp.route("/ping", methods=["GET"])
-def ping():
-    return jsonify({"message": "pong"}), 200

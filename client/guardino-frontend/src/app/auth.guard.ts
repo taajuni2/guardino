981f823b9a86c11f4f6902d971d1d0171ea8d96e
taajuni2 <nicolas.julier@swisscom.com>
2025-11-25 15:45:27 +0100

    adjusted env's

--- client/guardino-frontend/src/app/auth.guard.ts
+++ client/guardino-frontend/src/app/auth.guard.ts
@@ -9,6 +9,7 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(): boolean {
+    console.log('isAuthenticated', this.auth.isAuthenticated);
     if (this.auth.isAuthenticated) {
       return true;
     }
--- client/guardino-frontend/src/environment/environment.ts
+++ client/guardino-frontend/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'http://192.168.110.60:8000',
   wsUrl: 'ws://localhost:5000/',
   kafkaTopicEvents: 'agent-events',
   kafkaTopicAgent: 'agent-lifecycle'

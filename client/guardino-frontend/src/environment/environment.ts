e5a1ade34b0008302cc18e1b811b26bb9d81dd68
taajuni2 <nicolas.julier@swisscom.com>
2025-11-25 15:52:29 +0100

    added depends on kafka for backend service in compose

--- client/guardino-frontend/src/environment/environment.ts
+++ client/guardino-frontend/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.110.60:8000',
+  apiUrl: 'http://192.168.110.60:8000', // muss auf backend adresse der VM geändert werden oder localhost für lokale entwickung
   wsUrl: 'ws://localhost:5000/',
   kafkaTopicEvents: 'agent-events',
   kafkaTopicAgent: 'agent-lifecycle'

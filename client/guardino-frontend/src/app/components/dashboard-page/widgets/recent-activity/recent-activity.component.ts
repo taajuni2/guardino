03cdc2e05d539eecd493488eaaf5d63608b6dab3
taajuni2 <nicolas.julier@swisscom.com>
2025-12-08 18:16:33 +0100

    added frontend event service

--- client/guardino-frontend/src/app/components/dashboard-page/widgets/recent-activity/recent-activity.component.ts
+++ client/guardino-frontend/src/app/components/dashboard-page/widgets/recent-activity/recent-activity.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {EventService} from "../../../../services/event-service/event.service";
 
 @Component({
   selector: 'app-recent-activity',
   templateUrl: './recent-activity.component.html',
   styleUrl: './recent-activity.component.scss'
 })
-export class RecentActivityComponent {
+export class RecentActivityComponent implements  OnInit {
+constructor(private eventService: EventService) {
+}
 
+ngOnInit() {
+  this.eventService.getGroupedEvents().subscribe(groupedEvents => {
+    console.log(groupedEvents);
+  })
+}
 }

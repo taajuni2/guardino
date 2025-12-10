bb767b7009706149cd7ff2636f1d6148c1b7ee58
taajuni2 <nicolas.julier@swisscom.com>
2025-12-10 20:18:06 +0100

    wip websockets

--- client/guardino-backend/app/services/__init__.py
+++ client/guardino-backend/app/services/__init__.py
@@ -1,3 +1,3 @@
 from .agent_service import handle_register, handle_heartbeat, handle_generic_event
 from .kafka_consumer import consume_agent_messages
-from .websockets import agents_manager, events_manager
+from .websockets import websocker_manager

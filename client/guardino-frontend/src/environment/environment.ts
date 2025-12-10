b65f53ec5a64e0a3389bbd4bf520586585853874
taajuni2 <nicolas.julier@swisscom.com>
2025-12-10 19:56:12 +0100

    wip websockets

--- client/guardino-frontend/src/environment/environment.ts
+++ client/guardino-frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://192.168.110.60:8000', // muss auf backend adresse der VM geändert werden oder localhost für lokale Entwickung
-  wsUrl: 'ws://192.168.110.60/:8000',
+  wsUrl: 'ws://192.168.110.60:8000',
 };

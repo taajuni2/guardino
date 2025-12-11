9322640c0e11829365fdc43415a87e2e3aa3ae83
taajuni2 <nicolas.julier@swisscom.com>
2025-12-11 13:22:58 +0100

    wip

--- client/guardino-frontend/src/app/components/events-page/widgets/event-timeline/event-timeline.component.ts
+++ client/guardino-frontend/src/app/components/events-page/widgets/event-timeline/event-timeline.component.ts
@@ -1,14 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EventService, UnifiedEvent} from "../../../../services/event-service/event.service";
 
-interface EventItem {
-  id: string;
+interface TimelineEvent {
   title: string;
-  severity: 'info' | 'critical' | 'warning';
-  agent: string;
   agentName: string;
-  description?: string;
+  agent: string;
+  description: string | null;
   timestamp: string;
-  meta?: string;
+  severity: 'info' | 'warning' | 'critical';
 }
 
 @Component({
@@ -16,37 +15,27 @@ interface EventItem {
   templateUrl: './event-timeline.component.html',
   styleUrl: './event-timeline.component.scss'
 })
-export class EventTimelineComponent {
-  events: EventItem[] = [
-    {
-      id: '1',
-      title: 'Regular heartbeat received',
-      severity: 'info',
-      agent: 'DESK-001',
-      agentName: 'Marketing-PC-01',
-      description: 'Agent status: Online, CPU: 45%, Memory: 67%, Disk: 23%',
-      timestamp: '2024-10-06 14:32:15'
-    },
-    {
-      id: '2',
-      title: 'Suspicious file encryption activity detected',
-      severity: 'critical',
-      agent: 'SRV-005',
-      agentName: 'FileServer-Main',
-      description: 'Multiple files being encrypted in rapid succession. Pattern matches known ransomware behavior.',
-      timestamp: '2024-10-06 14:30:42'
-    },
-    {
-      id: '3',
-      title: 'Full system scan completed',
-      severity: 'info',
-      agent: 'DESK-045',
-      agentName: 'Dev-Workstation-03',
-      description: 'Scanned 245,678 files. No threats detected. Scan duration: 1h 23m',
-      timestamp: '2024-10-06 14:28:33'
-    }
-  ];
+export class EventTimelineComponent implements  OnInit {
+  events: TimelineEvent[] = [];
+  constructor(private eventService: EventService) {}
 
-  selectedSeverity = 'all';
-  selectedType = 'all';
+
+  ngOnInit(): void {
+    this.eventService.allEvents$.subscribe(rawEvents => {
+      this.events = rawEvents.map(ev => this.mapEvent(ev));
+      console.log(rawEvents)
+    });
+  }
+
+  private mapEvent(ev: UnifiedEvent): TimelineEvent {
+
+    return {
+      title: ev.summary ?? 'Unknown Event',
+      agentName: ev.agent_id ?? 'Unknown Agent',
+      agent: ev.agent_id ?? 'N/A',
+      description: ev.summary ?? null,
+      timestamp: new Date(ev.ts).toLocaleString(),
+      severity: (ev.severity as any) ?? 'info'
+    };
+  }
 }
